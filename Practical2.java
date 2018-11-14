a5a8e57595e2137b66ca823c8e510824e62c201b
Max <max.09@outlook.com>
2018-11-14 14:19:26 +0100

    create an evolution loop
    Where selection and reprodution happen

--- Practical2.java
+++ Practical2.java
@@ -59,13 +59,169 @@ public class Practical2 {
 
 
 
+		// do your own cool GA here
+		/**
+		 * Some general programming remarks and hints:
+		 * - A crucial point is to set each individual's fitness (by the setFitness() method) before sorting. When is an individual fit? 
+		 * 	How do you encode that into a double (between 0 and 1)? -- CHECK
+		 * - Decide when to stop, that is: when the algorithm has converged. And make sure you  terminate your loop when it does.
+		 * - print the whole population after convergence and print the number of generations it took to converge.
+		 * - print lots of output (especially if things go wrong).
+		 * - work in an orderly and structured fashion (use tabs, use methods,..)
+		 * - DONT'T make everything private. This will only complicate things. Keep variables local if possible
+		 * - A common error are mistakes against pass-by-reference (this means that you pass the 
+		 * 	address of an object, not a copy of the object to the method). There is a deepclone method included in the
+		 *  Individual class.Use it!
+		 * - You can compare your chromosome and your target string, using for eg. TARGET.charAt(i) == ...
+		 * - Check your integers and doubles (eg. don't use ints for double divisions).
+		 */
+
+
+
+	}// END MAIN
+
+
+
+	/*Give a fitness score to an individual between 0 and 1. 
+	The function function is the percentage of characters 
+	that match the target ( must be same character and same position).
+	*/
+	public static double calcFitness(Individual individu){
+		double fitness = 0 ; 
+		for (int i = 0 ; i < individu.chromosome.length; i++) {
+			//Checks if the character at position i is the same in target
+			if (individu.chromosome[i] == TARGET.charAt(i) ){
+				fitness = fitness + 1.0/individu.chromosome.length;
+			}
+		}
+		return fitness ;
+	}
+
+
+
+		//Select two parents at random in the mating pool
+
+	//Selects an individual at random in an array ( the population )
+	public static Individual select(Individual[] population) {
+		Random ran = new Random(); 
+		int x = ran.nextInt(population.length-1)  ;
+		System.out.println(population.length);
+		System.out.println("x : "+x);	
+		return population[x];
+	}
+
+	/**Performs crossover from two parents, returns a newborn.
+	It simply takes the first half of parent1 and second half from parent2 and merges the two parts.*/
+	public static Individual crossover(Individual parent1, Individual parent2) {
+
+		String dna1 = parent1.genoToPhenotype() ;
+		String dna2 = parent2.genoToPhenotype() ; 
+		String newDNA = new String(); 
+
+		//Spliting point
+		final int LENGTH_OF_DNA = dna1.length();
+		if (DEBUG) System.out.println("LENGTH_OF_DNA : "+ LENGTH_OF_DNA);
+		int split =  LENGTH_OF_DNA / 2 ; 
+		System.out.println("splitting point : "+ split);
+
+		// DNA as form of char list instead of String
+		char[] chromosome1 = parent1.getChromosome();
+		char[] chromosome2 = parent2.getChromosome();
+		char[] newchromosome = new char[LENGTH_OF_DNA];
+		int i = 0 ; 
+
+		System.out.println("Copying DNA from first parent...");
+		while (i<split) {
+			//Copy firsts characters from parent1
+			newchromosome[i] = chromosome1[i];
+
+			if (DEBUG) {
+				String b = String.valueOf(newchromosome);
+				System.out.println(b + "\n i : "+i);
+			} 
+			i++;
+		}
+		if (DEBUG) System.out.println("switching the second parent...");
+		while (i >= split && i < LENGTH_OF_DNA){
+			//Copy latter characters from parent2
+			newchromosome[i] = chromosome2[i];
+			if (DEBUG) {
+				String b = new String(newchromosome);
+				System.out.println(b + "\n i : "+i);
+			} 
+			i++;
+		}
+
+			//Create the child
+		Individual child = new Individual(newchromosome); 
+		if (DEBUG)  System.out.println("new child : " + child.genoToPhenotype());
+		return child; 
+	}
+
+	/**Exposes the child to gamma rays ( may result as an alteration of the DNA ...)
+	@param individu Indivudual that we are gonna expose to gamma rays and may mutate
+	@param rate Chance of a letter is altered ( between 0 and 1 )
+	*/
+	public static Individual mutation(Individual individu, double rate){
+		if (DEBUG ) System.out.println("Trying to mutate "+individu.genoToPhenotype() + " with rate "+ rate);
+
+
+		//Creating an alhabet ( A to Z + space) ( code reused )
+		for (char c = 'A'; c <= 'Z'; c++) {
+			alphabet[c - 'A'] = c;
+		}
+		alphabet[26] = ' ';
+
+		Random ran = new Random();
+		char[] dna = individu.getChromosome();
+		char[] newDNA = individu.getChromosome();
+		for (int i = 0 ; i< dna.length ; i++ ) {
+			double x = ran.nextDouble(); 
+			if (DEBUG) System.out.println("x = "+ x);
+			if (x < rate) {
+
+				//Mutation ! 
+				
+
+				//Gives a random letter
+				newDNA[i] =  alphabet[ran.nextInt(alphabet.length)]; 
+				System.out.println("mutation !  "+(i+1)+"nd element" +" becomes "+newDNA[i]);
+
+			} 
+			else {
+				//No mutation
+				newDNA[i] = dna[i];
+			}
+			i++;
+			
+		}//end for loop
+		Individual mutatedIndividual = new Individual(newDNA);
+
+		if (DEBUG)	System.out.println("Mutated individual : "+ mutatedIndividual.genoToPhenotype());
+		return mutatedIndividual;
+
+
+	}// end mutation method
+
+
+	//--------------------------------------------------//
+			//-----------EVOLUTION LOOP---------//
+	//--------------------------------------------------//
+
+
+		//Recursive method to make a loop
+	public static Individual[] evolution( Individual[] population ){
+		final boolean DEBUG = true ; 
+		int popSize =  population.length; 
 
 		//--------- SELECTION PHASE --------------//
 
 
+
+
 		//Calculate and assign fitness for every individual 
 		for (int i = 0; i < population.length; i++) {
-			population[i].setFitness(calcFitness(population[i]));
+			population[i].setFitness(Practical2.calcFitness(population[i]));
 		}
 		//Just DEBUG info in a external txt file
 		if (DEBUG) {	
@@ -203,152 +359,7 @@ public class Practical2 {
 
 			}
 		}
-
-
-
-
-
-		// do your own cool GA here
-		/**
-		 * Some general programming remarks and hints:
-		 * - A crucial point is to set each individual's fitness (by the setFitness() method) before sorting. When is an individual fit? 
-		 * 	How do you encode that into a double (between 0 and 1)? -- CHECK
-		 * - Decide when to stop, that is: when the algorithm has converged. And make sure you  terminate your loop when it does.
-		 * - print the whole population after convergence and print the number of generations it took to converge.
-		 * - print lots of output (especially if things go wrong).
-		 * - work in an orderly and structured fashion (use tabs, use methods,..)
-		 * - DONT'T make everything private. This will only complicate things. Keep variables local if possible
-		 * - A common error are mistakes against pass-by-reference (this means that you pass the 
-		 * 	address of an object, not a copy of the object to the method). There is a deepclone method included in the
-		 *  Individual class.Use it!
-		 * - You can compare your chromosome and your target string, using for eg. TARGET.charAt(i) == ...
-		 * - Check your integers and doubles (eg. don't use ints for double divisions).
-		 */
-
-
-
-	}// END MAIN
-
-
-
-	/*Give a fitness score to an individual between 0 and 1. 
-	The function function is the percentage of characters 
-	that match the target ( must be same character and same position).
-	*/
-	public static double calcFitness(Individual individu){
-		double fitness = 0 ; 
-		for (int i = 0 ; i < individu.chromosome.length; i++) {
-			//Checks if the character at position i is the same in target
-			if (individu.chromosome[i] == TARGET.charAt(i) ){
-				fitness = fitness + 1.0/individu.chromosome.length;
-			}
-		}
-		return fitness ;
+		return population;
 	}
 
-
-
-		//Select two parents at random in the mating pool
-
-	//Selects an individual at random in an array ( the population )
-	public static Individual select (Individual[] population) {
-		Random ran = new Random(); 
-		int x = ran.nextInt(population.length-1)  ;
-		System.out.println(population.length);
-		System.out.println("x : "+x);	
-		return population[x];
-	}
-
-	/**Performs crossover from two parents, returns a newborn.
-	It simply takes the first half of parent1 and second half from parent2 and merges the two parts.*/
-	public static Individual crossover(Individual parent1, Individual parent2) {
-
-		String dna1 = parent1.genoToPhenotype() ;
-		String dna2 = parent2.genoToPhenotype() ; 
-		String newDNA = new String(); 
-
-		//Spliting point
-		final int LENGTH_OF_DNA = dna1.length();
-		if (DEBUG) System.out.println("LENGTH_OF_DNA : "+ LENGTH_OF_DNA);
-		int split =  LENGTH_OF_DNA / 2 ; 
-		System.out.println("splitting point : "+ split);
-
-		// DNA as form of char list instead of String
-		char[] chromosome1 = parent1.getChromosome();
-		char[] chromosome2 = parent2.getChromosome();
-		char[] newchromosome = new char[LENGTH_OF_DNA];
-		int i = 0 ; 
-
-		System.out.println("Copying DNA from first parent...");
-		while (i<split) {
-			//Copy firsts characters from parent1
-			newchromosome[i] = chromosome1[i];
-
-			if (DEBUG) {
-				String b = String.valueOf(newchromosome);
-				System.out.println(b + "\n i : "+i);
-			} 
-			i++;
-		}
-		if (DEBUG) System.out.println("switching the second parent...");
-		while (i >= split && i < LENGTH_OF_DNA){
-			//Copy latter characters from parent2
-			newchromosome[i] = chromosome2[i];
-			if (DEBUG) {
-				String b = new String(newchromosome);
-				System.out.println(b + "\n i : "+i);
-			} 
-			i++;
-		}
-
-			//Create the child
-		Individual child = new Individual(newchromosome); 
-		if (DEBUG)  System.out.println("new child : " + child.genoToPhenotype());
-		return child; 
-	}
-
-	/**Exposes the child to gamma rays ( may result as an alteration of the DNA ...)
-	@param individu Indivudual that we are gonna expose to gamma rays and may mutate
-	@param rate Chance of a letter is altered ( between 0 and 1 )
-	*/
-	public static Individual mutation(Individual individu, double rate){
-		if (DEBUG ) System.out.println("Trying to mutate "+individu.genoToPhenotype() + " with rate "+ rate);
-
-
-		//Creating an alhabet ( A to Z + space) ( code reused )
-		for (char c = 'A'; c <= 'Z'; c++) {
-			alphabet[c - 'A'] = c;
-		}
-		alphabet[26] = ' ';
-
-		Random ran = new Random();
-		char[] dna = individu.getChromosome();
-		char[] newDNA = individu.getChromosome();
-		for (int i = 0 ; i< dna.length ; i++ ) {
-			double x = ran.nextDouble(); 
-			if (DEBUG) System.out.println("x = "+ x);
-			if (x < rate) {
-
-				//Mutation ! 
-				
-
-				//Gives a random letter
-				newDNA[i] =  alphabet[ran.nextInt(alphabet.length)]; 
-				System.out.println("mutation !  "+(i+1)+"nd element" +" becomes "+newDNA[i]);
-
-			} 
-			else {
-				//No mutation
-				newDNA[i] = dna[i];
-			}
-			i++;
-			
-		}//end for loop
-		Individual mutatedIndividual = new Individual(newDNA);
-
-		if (DEBUG)	System.out.println("Mutated individual : "+ mutatedIndividual.genoToPhenotype());
-		return mutatedIndividual;
-
-
-	}
 }
